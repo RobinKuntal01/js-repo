bc54844cb9d5813ef48e805a84e8fb627191dc5d
RobinKuntal01 <107622477+RobinKuntal01@users.noreply.github.com>
2024-02-15 19:22:08 +0000

    commit

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,19 @@
+const accountId = 12339032;
+let accountEmail = "robin@google.com";
+var accountPassword = "12345";
+accountCity = "Noida";
+
+// accountId = 2 cant change const variable
+
+console.log(accountId);
+
+/*
+Do not use "var" 
+because of functional use and block use 
+*/
+
+accountEmail = "rk@google.com";
+accountPassword = "126781199";
+accountCity = "Mumbai";
+
+console.table([accountId,accountEmail,accountPassword,accountCity])
