505e4fdf750ffdd00d85062cfd9bc02f6b08ca61
RobinKuntal01 <107622477+RobinKuntal01@users.noreply.github.com>
2024-02-28 12:31:07 +0000

    setInterval

--- 07_events/engine.js
+++ 07_events/engine.js
@@ -1,16 +1,20 @@
 let body = document.querySelector('body')
 
-
-
 let p = document.createElement('div')
-
-
 
 body.addEventListener('click', function(e){
   let Xaxis = e.clientX
  let Yaxis = e.clientY
 
  console.log(Xaxis, Yaxis);
- p.setAttribute('style', `height: 100px; width: 100px; background-color: red; border-radius:50%; left:${Xaxis-10}px; top:${Yaxis-10}px; position:relative;`)
+//  p.setAttribute('style', `height: 100px; width: 100px; background-color: red; border-radius:50%; left:${Xaxis-10}px; top:${Yaxis-10}px; position:relative;`)
+
+p.style.height = "100px"
+p.style.width = "100px"
+p.style.backgroundColor = "Red"
+p.style.top = `${Yaxis-10}px`
+p.style.left = `${Xaxis-10}px`
+p.style.position = 'relative'
+
  body.appendChild(p)
 })
